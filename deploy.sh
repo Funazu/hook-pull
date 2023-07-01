25080f5352a44a1373a95d616467acffdbc03cd8
UriSuzy <webmaulana1@gmail.com>
2023-07-01 12:46:23 +0700

    create make-user command and update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
 composer install
 
+cp .env.example .env
+
+echo Username:
+read user
+
+echo Password:
+read pass
+
 php artisan key:generate
 
 php artisan migrate
+
+php artisan app:make-user $user $pass
+
+echo Deploy successfully!
